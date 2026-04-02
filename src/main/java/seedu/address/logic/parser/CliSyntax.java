67335ca336a109bde5b59e4eeeac83a3b249ccd7
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-02 11:31:13 +0800

    Register newtag prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,6 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ROOM = new Prefix("r/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_NEWTAG = new Prefix("-newtag");
 
     public static final Prefix PREFIX_COMMENT = new Prefix("c/");
 }
